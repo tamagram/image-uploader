0a83fbd6b845ee14ccb57e7d9c6723bbcf65d358
tamagram <tamagram64@gmail.com>
2021-05-23 11:41:34 +0900

    :hammer: Add ESLint and Prettier.

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        // No need to import React in Next.js
+        "react/prop-types": "off",
+        "react/react-in-jsx-scope": "off"
+    }
+};
